027bb91b17874156bb73f747ed9ea814f9748d00
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-06-16 10:47:31 +0200

    exclude securityConfig pour l'instant

--- src/main/java/com/mediscreen/Mediscreen/MediscreenApplication.java
+++ src/main/java/com/mediscreen/Mediscreen/MediscreenApplication.java
@@ -2,8 +2,13 @@ package com.mediscreen.Mediscreen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+(exclude = {
+		SecurityAutoConfiguration.class,
+		}
+)
 public class MediscreenApplication {
 
 	public static void main(String[] args) {

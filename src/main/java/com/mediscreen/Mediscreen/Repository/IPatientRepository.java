7863fe5e858cf856c0fe5e32d92ed9af5cda6c2f
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-06-23 14:52:45 +0200

    changement de Option a List

--- src/main/java/com/mediscreen/Mediscreen/Repository/IPatientRepository.java
+++ src/main/java/com/mediscreen/Mediscreen/Repository/IPatientRepository.java
@@ -4,9 +4,8 @@ import com.mediscreen.Mediscreen.model.PatientEntity;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IPatientRepository extends CrudRepository<PatientEntity, Integer> {
 
-    Optional<PatientEntity> findPatientByLastName(String lastName);
+    List<PatientEntity> findPatientByLastName(String lastName);
 }

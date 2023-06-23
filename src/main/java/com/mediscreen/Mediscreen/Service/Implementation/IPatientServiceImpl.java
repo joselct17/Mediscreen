b0d628975555ead66a36a6b8cc22f67c279bfcf3
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-06-23 14:53:19 +0200

    rajout de updatedPatientEntity

--- src/main/java/com/mediscreen/Mediscreen/Service/Implementation/IPatientServiceImpl.java
+++ src/main/java/com/mediscreen/Mediscreen/Service/Implementation/IPatientServiceImpl.java
@@ -50,23 +50,52 @@ public class IPatientServiceImpl implements IPatientService {
             return patientById.get();
         } else {
             logger.info("Patient with id{} is not found in IPatientServiceImpl,", id);
-            throw new RuntimeException("Patient with id:{%s} doesn't exist in DB!");
+            throw new RuntimeException("Patient with id:{} doesn't exist in DB!");
         }
     }
 
+
     @Override
-    public PatientEntity findPatientByLastName(String lastName) {
-        logger.info("find Patient by lastName{} call in IPatientServiceImpl", lastName);
-        Optional<PatientEntity> patientEntityByLastName = patientRepository.findPatientByLastName(lastName);
-       logger.info("the patient with de lastName{} has been find", lastName);
-        return patientEntityByLastName.get();
+    public List<PatientEntity> findPatientByLastName(String lastName) {
+        logger.debug("getPatientByLastName from PatientServiceImpl starts here");
+        List<PatientEntity>patient = patientRepository.findPatientByLastName(lastName);
+
+        if (patient.isEmpty()) {
+            logger.error("Patient doesn't exist in DB with lastName:{{}}", lastName);
+            throw new RuntimeException("Patient with lastName:{%s} doesn't exist in DB!".formatted(lastName));
+        }
+        logger.info("Patient has been retrieved successfully by lastName:{{}}, from PatientServiceImpl", lastName);
+        return patient;
     }
+
 
     @Override
     public PatientEntity savePatientEntity(PatientEntity patientEntity) {
-        logger.debug("savePatientEntity method starts here, from IPatientEntity");
+        logger.debug("savePatientEntity method starts here, from IPatientServiceImpl");
         PatientEntity patientSaved = patientRepository.save(patientEntity);
         logger.info("Patient with Id:{} has been successfully save", patientEntity.getId());
         return patientSaved;
     }
+
+    @Override
+    public PatientEntity updatedPatientEntity(Integer id, PatientEntity patientEntity) {
+        logger.debug("updatedPatientEntityById method starts here, from IPatientServiceImpl");
+        Optional<PatientEntity> patientById = patientRepository.findById(id);
+        if (patientById.isPresent()) {
+            PatientEntity patientUpdated = patientById.get();
+            patientById.get().setId(patientEntity.getId());
+            patientById.get().setName(patientEntity.getName());
+            patientById.get().setPhone(patientEntity.getPhone());
+            patientById.get().setSex(patientEntity.getSex());
+            patientById.get().setBirthDate(patientEntity.getBirthDate());
+            patientById.get().setLastName(patientEntity.getLastName());
+            patientById.get().setAddress(patientEntity.getAddress());
+            patientRepository.save(patientById.get());
+            return patientUpdated;
+        } else {
+            logger.debug("Any patient  exist with id:{} in DB!", id);
+            throw new RuntimeException("Any patient  exist with id:{%s}".formatted(id));
+        }
+    }
 }
+
